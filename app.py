8d2ae8ff8ba7dae8de6077b945ddae60361d8e23
Jake-Egbert <jake@devpipeline.com>
2024-06-24 11:39:51 -0600

    adds cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 from flask_marshmallow import Marshmallow
@@ -21,19 +22,21 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"{database_scheme}{database_user}@{database_address}:{database_port}/{database_name}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-init_db(app,db)
+init_db(app, db)
 ma = Marshmallow(app)
 
 register_blueprints(app)
 
+
 def create_tables():
     with app.app_context():
-        print ("creating tables...")
+        print("creating tables...")
         db.create_all()
         print("tables created successfully")
 
+
+CORS(app)
 create_tables()
 
 if __name__ == "__main__":
-    app.run (host = flask_host, port = flask_port)
-
+    app.run(host=flask_host, port=flask_port)
